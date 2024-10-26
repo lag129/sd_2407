6d704bf83a956ea9321bc40a5a3de00ca92da0b0
haruhide39 <sc91rdkpsng3dbuneeew@docomo.ne.jp>
2024-10-26 15:29:25 +0900

    UI_meisi

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { IncorrectTextBox } from './ui/IncorrectTextBox';
+import { addsyumi } from './ui/addsyumi';
 
 const App = () => {
   const useState = React.useState;
   const [state, setState] = useState(0);
   const handleClick = () => setState(state + 1);
+
+
 
   return (
     <div>
@@ -15,7 +18,11 @@ const App = () => {
       <div>
         <button onClick={handleClick}>count up</button>
       </div>
-      <IncorrectTextBox label="Input your fisrt name." />
+        <IncorrectTextBox label="名前：" />
+        <IncorrectTextBox label="所属：" />
+        <IncorrectTextBox label="趣味などを入力" />
+        <button onClick={addsyumi}>追加</button>
+        
     </div>
   );
 };
