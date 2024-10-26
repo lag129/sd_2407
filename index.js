9e97cde158a172abe2471d0a0250e4393a29ac56
lag129 <65485662+lag129@users.noreply.github.com>
2024-10-26 15:46:42 +0900

    react-router-domを導入

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { IncorrectTextBox } from './ui/IncorrectTextBox';
-// import { addsyumi } from './ui/addsyumi';
 
-const App = () => {
+const Home = () => {
   const useState = React.useState;
   const [state, setState] = useState(0);
   const handleClick = () => setState(state + 1);
-
-
 
   return (
     <div>
@@ -21,11 +19,19 @@ const App = () => {
       <IncorrectTextBox label="名前：" />
       <IncorrectTextBox label="所属：" />
       <IncorrectTextBox label="趣味などを入力" />
-      <button onClick={addsyumi}>追加</button>
-
+      {/* <button onClick={addsyumi}>追加</button> */}
     </div>
   );
 };
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* 他のルートを追加する場合はここに記述 */}
+    </Routes>
+  </Router>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<App />);
