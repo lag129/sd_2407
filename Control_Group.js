b798cc150ed2d1de7a0c2d53b4943c740a7a36d0
Yasuda-0215 <186050766+Yasuda-0215@users.noreply.github.com>
2024-10-26 15:06:51 +0900

    グループの作成,参加等の関数を追加しました． 仮なので要都度修正かも

---
+++ Control_Group.js
@@ -0,0 +1,35 @@
+import { generateSix } from './IDgenerate.js';
+class Group{
+  Group(name)
+  {
+    this.name = name;
+    this.id = generateSix();
+    this.members = [];
+  }
+
+  addMember(member)
+  {
+    this.members.push(member);
+  }
+
+  createGroup(name)
+  {
+    const _group = new Group(name);
+    this.this.groups[group.id] = group;
+    return group.id;
+  }
+
+  joinGroup(groupId,member){
+    if(this.groups[groupId])
+    {
+      this.groups[groupId].addMember(member);
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  getGroup(groupId){
+    return this.groups[groupId];
+  }
+}
