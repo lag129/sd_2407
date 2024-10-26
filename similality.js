eb391e6a90b54a9392a8bc5e17d39e147a1e7156
Agpudding <s2212220@st.tohtech.ac.jp>
2024-10-26 18:53:18 +0900

    修正版３

--- similality.js
+++ similality.js
@@ -5,8 +5,8 @@ mylen = myTags.length;
 yourlen = yourTags.length;
 
 function Find_Smilarity(){
-  for(const len1=0;len1>0;len1--){
-    for(const len2=0;len2>0;len2--){
+  for(const len1=0;len1>mylen;len1++){
+    for(const len2=0;len2>yourlen;len2++){
       if(mylen[len1] == yourlen[len2]){
         simicount++;
       }
