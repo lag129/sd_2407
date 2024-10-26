dbbedf60e9e35573291a42485730d4fa1ba29b7a
lag129 <65485662+lag129@users.noreply.github.com>
2024-10-26 23:19:11 +0900

    関数名の変更とJSDocの追加

---
+++ logic/findSimilarity.js
@@ -0,0 +1,29 @@
+// @ts-check
+"use strict";
+
+// テストデータ
+// const tags1 = ['aaa','bbb','CCC'];
+// const tags2 = ['aaa','bbb','CCC'];
+
+/**
+ * 2つのタグの配列を比較して、類似度を計算する
+ * 
+ * @param {Array<string>} mytags 
+ * @param {Array<string>} yourtags 
+ * @returns int 類似度
+ */
+const findSmilarity = (mytags, yourtags) => {
+  // 2つの配列の共通要素を出力   
+  const result = mytags.filter(element => yourtags.includes(element));
+  // 共通の要素の数を計算
+  const commonCount = result.length;
+
+  // 配列の最大長さを計算
+  const maxLength = Math.max(mytags.length, yourtags.length);
+
+  console.log((commonCount / maxLength) * 100);
+  // 類似度をパーセンテージで計算   
+  return (commonCount / maxLength) * 100;
+}
+
+// console.log(findSmilarity(tags1,tags2));
