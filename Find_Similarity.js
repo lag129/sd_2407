1dae22e793123dbdb13150e0809e362c4b54318e
Yasuda-0215 <186050766+Yasuda-0215@users.noreply.github.com>
2024-10-26 19:01:12 +0900

    ccc

--- Find_Similarity.js
+++ Find_Similarity.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-///テストデータ
+//テストデータ
 mytags = ['aaa','bbb','CCC'];
 yourtags = ['aaa','DDD','BBB','CCC'];
 Find_Smilarity(mytags,yourtags)
@@ -14,18 +13,4 @@ Find_Smilarity(mytags,yourtags)
    
   // 類似度をパーセンテージで計算   
   return (commonCount / maxLength) * 100;
-=======
-//import { Group_outputInfo } from './Group_outputInfo.js';
-//mytags = Group_outputInfo();
-
-Find_Smilarity(mytags,yourtags)
-{
-  const similarity = 0.0;
-  //2つの配列の共通要素を出力   ex(この場合):[index:2 mytags:x, yourtags:y]
-  const result = compareArrays(mytags, yourtags);
-  //分子 = yourtagsとの共通個数+mytagsとの共通個数/mytagsの長さ+yourtags長さ
-  //最後に2で割っていい感じになってほしい
-  similarity = (result[1]+result[2]/mytags.length+yourtags.length)/2.0 ;
-  return similarity*100;
->>>>>>> eb391e6a90b54a9392a8bc5e17d39e147a1e7156
 }
