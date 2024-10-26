064ff7c12c93b71496568b4e0118a2ea0593916c
Yasuda-0215 <186050766+Yasuda-0215@users.noreply.github.com>
2024-10-26 20:13:31 +0900

    類似度処理Fix(?)

--- Find_Similarity.js
+++ Find_Similarity.js
@@ -1,7 +1,7 @@
 //テストデータ
-mytags = ['aaa','bbb','CCC'];
-yourtags = ['aaa','DDD','BBB','CCC'];
-Find_Smilarity(mytags,yourtags)
+tags1 = ['aaa','bbb','CCC'];
+tags2 = ['aaa','bbb','CCC'];
+function Find_Smilarity(mytags,yourtags)
 {
   //2つの配列の共通要素を出力   
   const result = mytags.filter(element => yourtags.includes(element));
@@ -11,6 +11,9 @@ Find_Smilarity(mytags,yourtags)
   // 配列の最大長さを計算
   const maxLength = Math.max(mytags.length, yourtags.length);
    
+  console.log((commonCount / maxLength) * 100);
   // 類似度をパーセンテージで計算   
   return (commonCount / maxLength) * 100;
 }
+
+//console.log(Find_Smilarity(tags1,tags2));
