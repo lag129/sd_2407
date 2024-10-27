16bd69dc1455c4f266107ca4be6a0f0b42f79dd4
Agpudding <s2212220@st.tohtech.ac.jp>
2024-10-27 12:41:43 +0900

    すこし修正

--- components/footer.js
+++ components/footer.js
@@ -6,12 +6,12 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 東北工大. </p>
+      <p>&copy; 2024 チーム東北工大. </p>
       <nav>
         <ul>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="/privacy">Privacy Policy</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Privacy Policy</a></li>
         </ul>
       </nav>
     </footer>
