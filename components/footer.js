5918b4a40c2798f89c42544eb580b255aa2e14d1
Agpudding <s2212220@st.tohtech.ac.jp>
2024-10-27 13:32:34 +0900

    footer

--- components/footer.js
+++ components/footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
       <p>&copy; 2024 チーム東北工大. </p>
       <nav>
         <ul>
-          <li className = {classes.text}><a href="#">About</a></li>
-          <li className = {classes.text}><a href="#">Contact</a></li>
-          <li className = {classes.text}><a href="#">Privacy Policy</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Privacy Policy</a></li>
         </ul>
       </nav>
     </footer>
