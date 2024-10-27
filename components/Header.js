7710a4149f2be39b3aad0de259cad52a1587759e
Yasuda-0215 <186050766+Yasuda-0215@users.noreply.github.com>
2024-10-27 10:41:54 +0900

    HeaderにCSSを対応させました

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //↓CSSファイル名を適切に変えてください
-import classes from "Header.css";
+import classes from './Header.css';
 //文字を押せば対象ページに行けるようにしたいので#に適切な変更を加えてください
 const Header = () => {
   return(
     <header>
-      <h1><a herf = "#">なにはな</a></h1>
+      <h1><a herf = "#"><img> src = </img></a></h1>
       <nav>
         <ul>
-          <li className = "header-item"><a href= "#">名刺の編集</a></li>
-          <li className = "header-item"><a href="#">グループを作る</a></li>
-          <li className = "header-item"><a href="#">グループに参加</a></li>
+          <li className = {classes.text}><a href= "#">名刺の編集</a></li>
+          <li className = {classes.text}><a href="#">グループを作る</a></li>
+          <li className = {classes.text}><a href="#">グループに参加</a></li>
         </ul>
       </nav>
     </header>
