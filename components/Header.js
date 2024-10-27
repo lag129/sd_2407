b9d3580dbb5e3f346e736f92ebd2e6698ea71559
Yasuda-0215 <186050766+Yasuda-0215@users.noreply.github.com>
2024-10-27 12:03:55 +0900

    試験的にプッシュ

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //↓CSSファイル名を適切に変えてください
 import classes from './Header.css';
+
+import logo from "../img/An1_png.png";
 //文字を押せば対象ページに行けるようにしたいので#に適切な変更を加えてください
 const Header = () => {
   return(
     <header>
-      <h1><a herf = "#"><img> src = </img></a></h1>
+      <h1 className={classes.img}><img src = {logo} alt = "ロゴマーク"></img></h1>
       <nav>
         <ul>
           <li className = {classes.text}><a href= "#">名刺の編集</a></li>
