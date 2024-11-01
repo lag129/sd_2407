5ae417fb3d6171751e178aecc7bdc6286ec32d8b
lag129 <65485662+lag129@users.noreply.github.com>
2024-11-01 17:55:18 +0900

    footer.jsxで使用していない箇所の削除

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,13 +6,6 @@ const Footer = () => {
   return (
     <footer className="Footer">
       <p>&copy; 2024 チーム東北工大. </p>
-      <nav>
-        <ul>
-          {/* <li><a href="#">About</a></li> */}
-          {/* <li><a href="#">Contact</a></li> */}
-          {/* <li><a href="#">Privacy Policy</a></li> */}
-        </ul>
-      </nav>
     </footer>
   );
 };
