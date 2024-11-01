48a59ea170b7beb11451b92add426a570c5c307f
lag129 <65485662+lag129@users.noreply.github.com>
2024-11-01 17:42:35 +0900

    HeaderのCSSを変更

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,12 @@
 "use strict";
 
-import styles from "./Header.module.css";
 import logo from "../assets/logo.png";
-//文字を押せば対象ページに行けるようにしたいので#に適切な変更を加えてください
-//各項目を横並びにしたいです
+
 const Header = () => {
   return (
-    <header className={styles.button_Header}>
-
-      <img src={logo} width="300" height="200" ></img>
-
+    <header>
+      <img src={logo} width="200" ></img>
     </header>
-
   );
 };
 
